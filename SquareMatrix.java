22a82393c1d64c825817f3908fc2f88fe86990b9
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-09-18 10:36:27 +0530

    Add files via upload

---
+++ SquareMatrix.java
@@ -0,0 +1,8 @@
+public class SquareMatrix {
+public static void main(String[] args) {
+int square[][]= {{1,3,5},{2,4,6}};
+System.out.println("Your Original Matrix: ");
+for(int i = 0; i < 2; i++){
+for(int j = 0; j < 3; j++){
+System.out.print(square[i][j] + " ");
+}
